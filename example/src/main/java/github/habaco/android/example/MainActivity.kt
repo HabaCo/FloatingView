83f0fc020304f0fa06ddb0f4e551fc528d518393
HabaCo <way6114@hotmail.com>
2019-10-19 02:59:34 +0800

    add preview3

--- example/src/main/java/github/habaco/android/example/MainActivity.kt
+++ example/src/main/java/github/habaco/android/example/MainActivity.kt
@@ -1,10 +1,9 @@
 package github.habaco.android.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import github.habaco.android.view.floating.FloatingUtil
 import github.habaco.android.view.floating.FloatingView
 import kotlinx.android.synthetic.main.activity_main.*
